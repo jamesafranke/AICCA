2ef39256bbc3245332a5a2dcbbec4c9c1011d8a3
jamesafranke <jfranke@uchicago.edu>
2022-10-28 17:13:23 -0500

    change to notebook

--- sc_deck_size.jl
+++ sc_deck_size.jl
@@ -6,18 +6,17 @@ using ProgressMeter
 
 ################################ Cloud classes #################################
 dfc = DataFrame(type_ = "high", idn = 1, Label = [1,2,3,4,5,6,7,8,9,10,11,12,17])
+append!(dfc, DataFrame( type_ = "mclosed", idn = 1, Label = [13,14,15,16] ) )  #How high are these when they occur in the subtropics???
 append!(dfc, DataFrame( type_ = "sparse", idn = 1, Label = [19,22,23,20,27,29,36,37,38,39,40,41] ) )
-append!(dfc, DataFrame( type_ = "open", idn = 1, Label = [0,18,25,26,30,32,34,42] ) )
+append!(dfc, DataFrame( type_ = "open",   idn = 1, Label = [0,18,25,26,30,32,34,42] ) )
 append!(dfc, DataFrame( type_ = "closed", idn = 0, Label = [21,24,28,31,33,35]  ) )
-append!(dfc, DataFrame( type_ = "mclosed", idn = 1, Label = [13,14,15,16] ) ) #How high are these when they occur in the subtropics???
 
 ################################ process years to get sc deck size #################################
 function process_year(year, startlat, endlat, startlon, endlon, region)
-    df = CSV.read( "AICCA/data/processed/$(year)_subtropic.csv", dateformat="yyyy-mm-dd HH:MM:SS", DataFrame )
+    df = CSV.read( "AICCA/data/processed/subtrop/$(year)_subtropic.csv", dateformat = "yyyy-mm-dd HH:MM:SS", DataFrame )
     @subset!(df, :lat .> startlat, :lat .< endlat, :lon .> startlon, :lon .< endlon) 
-    df.lat = floor.(df.lat)
-    df.lon = floor.(df.lon)
-    df = @orderby(df, :Timestamp)
+    df.lat = floor.(df.lat);  df.lon = floor.(df.lon)
+    df = orderby(df, :Timestamp)
     leftjoin!(df, dfc, on = :Label)
 
     ################## make daily raster for single basin ##########################
@@ -39,10 +38,8 @@ function process_year(year, startlat, endlat, startlon, endlon, region)
         for y_shift in [-1,1]      
             for x_shift in [-1,1]
                 temp = circshift( m, (y_shift, x_shift) )
-                temp[1,:] .= -1  
-                temp[end,:] .= -1  
-                temp[:,1] .= -1  
-                temp[:,end] .= -1  
+                temp[1,:] .= -1 ; temp[end,:] .= -1  # dont allow rollover from other edge of domain
+                temp[:,1] .= -1 ; temp[:,end] .= -1  # dont allow rollover from other edge of domain 
                 m[m.<0] .= temp[m.<0]
             end 
         end
@@ -63,9 +60,6 @@ function process_year(year, startlat, endlat, startlon, endlon, region)
 end 
 
 for year in 2003:2021 process_year( year, -45, 0, -115, -70, "spacific" ) end
-
 for year in 2003:2021 process_year( year, 0, 45, -140, -95, "n_pacific" ) end
-
 for year in 2003:2021 process_year( year, -40, 5, -27, 18, "africa" ) end
-
 for year in 2003:2021 process_year( year, -45, 0, 75, 120, "indian" ) end
