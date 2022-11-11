2883853ba9fdb8d7426a44ccdc8aeb6e9b26baea
jamesafranke <jfranke@uchicago.edu>
2022-11-11 12:01:41 -0600

    merge with climate update

--- merge_with_clim.jl
+++ merge_with_clim.jl
@@ -6,35 +6,45 @@ path = joinpath(pwd(),"data/processed/subtropic/")
 fl = filter( !contains(".DS"), readdir(path) )
 df = DataFrame()
 for i in fl append!( df, CSV.read( joinpath(path, i), dateformat="yyyy-mm-dd HH:MM:SS", DataFrame ) ) end
-df.lat = floor.(df.lat) .+ 0.5; df.lon = floor.(df.lon) .+ 0.5
+df.lat = floor.(df.lat) .+ 0.5
+df.lon = floor.(df.lon) .+ 0.5
 @select! df :Timestamp :lat :lon :Label :platform
 @transform! df :year=Year.(:Timestamp) :month=Month.(:Timestamp)
 
 ## Monthly vertical velocity from ERA5 ##
-dfw = CSV.read( joinpath(pwd(),"data/processed/era5_700hpa_vertical_velocity.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame ) 
+dfw = CSV.read( joinpath(pwd(),"data/processed/era5_700hpa_w.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame ) 
 @transform! dfw :year=Year.(:time) :month=Month.(:time)
 @select! dfw :year :month :lat :lon :w
-dftemp = @subset dfw :lon .> 180
+dftemp = @subset dfw :lon.>180
 dftemp.lon .-= 360
-@subset! dfw :lon .< 180
+@subset! dfw :lon.<180
 append!( dfw, dftemp )
 
 ## Monthly lower tropospheric stability from ERA5 (700hpa potential temp - 1000hpa potential temp) ##
-dfl = CSV.read( joinpath(pwd(),"data/processed/era5_LTS.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame ) 
+dfl = CSV.read( joinpath(pwd(),"data/processed/era5_lts.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame ) 
 @transform! dfl :year=Year.(:time) :month=Month.(:time)
 @select! dfl :year :month :lat :lon :lts
-dftemp = @subset dfl :lon .> 180
+dftemp = @subset dfl :lon.>180
 dftemp.lon .-= 360
-@subset! dfl :lon .< 180
+@subset! dfl :lon.<180
 append!( dfl, dftemp )
+
+## Monthly boundary layer height from ERA5 ##
+dfb = CSV.read( joinpath(pwd(),"data/processed/era5_blh.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame ) 
+@transform! dfb :year=Year.(:time) :month=Month.(:time)
+@select! dfb :year :month :lat :lon :blh
+dftemp = @subset dfb :lon.>180
+dftemp.lon .-= 360
+@subset! dfb :lon.<180
+append!( dfb, dftemp )
 
 ## Monthly sst from NOAA NCEP Reanaluysis ##
 dfs = CSV.read( joinpath(pwd(),"data/processed/noaa_ncep_sst.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame )
 @transform! dfs :year=Year.(:time) :month=Month.(:time) 
 @select! dfs :year :month :lat :lon :sst
-dftemp = @subset dfs :lon .> 180
+dftemp = @subset dfs :lon.>180
 dftemp.lon .-= 360
-@subset! dfs :lon .< 180
+@subset! dfs :lon.<180
 append!( dfs, dftemp )
 
 ## Monthly AOT from AHVRR satellite ##
@@ -45,10 +55,12 @@ dfa = CSV.read( joinpath(pwd(),"data/processed/avhrr_aot_month.csv"), dateformat
 ## join em all up by location, month, and year ##
 leftjoin!( df, dfw, on = [:year, :month, :lat, :lon] )
 leftjoin!( df, dfl, on = [:year, :month, :lat, :lon] )
+leftjoin!( df, dfb, on = [:year, :month, :lat, :lon] )
 leftjoin!( df, dfs, on = [:year, :month, :lat, :lon] )
 leftjoin!( df, dfa, on = [:year, :month, :lat, :lon] )
 
 ## write dataframe with lable, sst, aot, and w to csv ##
-@select! df :Timestamp :lat :lon :Label :platform :w :lts :sst :aot1 
+@select! df :Timestamp :lat :lon :Label :platform :w :lts :blh :sst :aot1 
 df = @orderby df :Timestamp
-CSV.write( joinpath(pwd(),"data/processed/all_subtropic_label_w_lts_sst_aot.csv" ), df, index = false)
+CSV.write( joinpath(pwd(),"data/processed/all_subtropic_label_with_climate.csv" ), df, index = false)
+
