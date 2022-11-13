41b51ea344d3ca1f93694aa65b22f7edea2b38ec
jamesafranke <jfranke@uchicago.edu>
2022-11-13 14:24:07 -0600

    file rename

---
+++ 01_daily_merge_with_climate.jl
@@ -0,0 +1,46 @@
+using CSV, DataFrames, DataFramesMeta, Dates 
+if occursin("AICCA", pwd()) == false cd("AICCA") else end
+
+## Load in the class data (from only the tropics) and merge with # merge class data with sst, lst, subsidence, and aerosol optical ##
+path = joinpath(pwd(),"data/processed/subtropic/")
+fl = filter( !contains(".DS"), readdir(path) )
+df = DataFrame()
+for i in fl append!( df, CSV.read( joinpath(path, i), dateformat="yyyy-mm-dd HH:MM:SS", DataFrame ) ) end
+df.lat = floor.(df.lat) .+ 0.5
+df.lon = floor.(df.lon) .+ 0.5
+@select! df :Timestamp :lat :lon :Label :platform
+@transform! df :year=Year.(:Timestamp) :day=Dayofyear.(:Timestamp)
+
+## lower tropospheric stability from ERA5 (700hpa potential temp - 1000hpa potential temp) ##
+dfl = CSV.read( joinpath(pwd(),"data/processed/era5_lts.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame ) 
+@transform! dfl :year=Year.(:time) :day=Dayofyear.(:time)
+@select! dfl :year :day :lat :lon :lts
+dftemp = @subset dfl :lon.>180
+dftemp.lon .-= 360
+@subset! dfl :lon.<180
+append!( dfl, dftemp )
+
+## Monthly boundary layer height from ERA5 ##
+dfb = CSV.read( joinpath(pwd(),"data/processed/era5_blh.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame ) 
+@transform! dfb :year=Year.(:time) :day=Dayofyear.(:time)
+@select! dfb :year :day :lat :lon :blh
+dftemp = @subset dfb :lon.>180
+dftemp.lon .-= 360
+@subset! dfb :lon.<180
+append!( dfb, dftemp )
+
+## Monthly AOT from AHVRR satellite ##
+dfa = CSV.read( joinpath(pwd(),"data/processed/avhrr_aot.csv"), dateformat="yyyy-mm-ddTHH:MM:SS.s", DataFrame )
+@transform! dfa :year=Year.(:time) :day=Dayofyear.(:time)
+@select! dfa :year :day :lat :lon :aot1
+
+## join em all up by location, month, and year ##
+leftjoin!( df, dfl, on = [:year, :day, :lat, :lon] )
+leftjoin!( df, dfb, on = [:year, :day, :lat, :lon] )
+leftjoin!( df, dfa, on = [:year, :day, :lat, :lon] )
+
+## write dataframe with lable, sst, aot, and w to csv ##
+@select! df :Timestamp :lat :lon :Label :platform :lts :blh :aot1 
+df = @orderby df :Timestamp
+CSV.write( joinpath(pwd(),"data/processed/all_subtropic_label_climate_day.csv" ), df, index = false)
+
--- 02_figure2.jl
+++ 02_figure2.jl
@@ -52,6 +52,54 @@ ylims!(212.5, 1425-12.5)
 png("./figures/heatmap.png")
 
 
+temp = @subset dfp :Label.!=0
+temp = @by temp [:lat, :lon] :counts=size(:lat)[1]
+temp = @orderby temp :counts rev=true
+temp = @subset temp :lat.<0
+
+temp = @subset dfp :lat.==35.5 :lon.==-134.5
+
+temp = @subset dfp :lat.==-33.5 :lon.==-93.5
+dfc = @chain temp begin  
+    dropmissing( [:lts, :blh] )
+    @transform :ltsbin=round.(:lts.*2, digits=0)./2 :blhbin=round.(:blh.*4, digits=-2) ./4
+    @by [:ltsbin, :blhbin, :Label] :counts=size(:lat)[1]
+    @orderby :counts rev=true
+    @aside dft = @subset _ :Label.!=0 
+    @aside dft = @by dft [:ltsbin, :blhbin] :nonzeroclass=last(:Label)
+    @by [:ltsbin, :blhbin] :maxclass=last(:Label) :maxcount=last(:counts) :total=sum(:counts)
+    leftjoin( dft, on=[:ltsbin, :blhbin] )
+    @rtransform :plotclass= :maxcount/:total>0.3 ? :maxclass : :nonzeroclass 
+    @subset :total.>50
+end
+
+colorclass = [25, 6, 27, 8, 40, 30, 32, 33, 36, 35]
+otherclass = [1,2,3,4,5,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,26,28,29,31,34,37,38,39,41,42]
+colors = cgrad(:Hiroshige, 10, categorical = true)
+
+temp = @subset dfc :plotclass.==0
+scatter(temp.ltsbin, temp.blhbin, markershape = :square, markersize = 5, markeralpha = 1, markercolor = :lightgray, 
+    markerstrokewidth = 0.75, markerstrokecolor=:lightgray, size=(500,550), grid = false, leg=false, dpi=800)
+
+for (i, class) in enumerate(colorclass)
+    temp = @subset dfc :plotclass.==class
+    if size(temp)[1] > 0
+        scatter!( temp.ltsbin, temp.blhbin, markershape = :square,
+        markersize = 5, markeralpha = 1, markercolor = colors[i], 
+        markerstrokewidth = 0.75, markerstrokecolor=:lightgray)
+    end
+end
+
+temp = @rsubset dfc :plotclass in otherclass
+scatter!(temp.ltsbin, temp.blhbin, markershape = :square, markersize = 5, markeralpha = 1, markercolor = :gray, 
+    markerstrokewidth = 0.75, markerstrokecolor=:gray )
+
+xlims!(10.25, 30)
+ylims!(212.5, 1425-12.5)
+png("./figures/heatmap_SP_gridcell.png")
+
+
+
 
 
 
