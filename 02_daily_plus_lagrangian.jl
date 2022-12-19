f85696bbe190d485e6212e05dadcb4b8a84a53fd
jamesafranke <jfranke@uchicago.edu>
2022-12-19 08:39:41 -0600

    lagrangian update

--- 02_daily_plus_lagrangian.jl
+++ 02_daily_plus_lagrangian.jl
@@ -5,3 +5,21 @@ if occursin("AICCA", pwd()) == false cd("AICCA") else end
 
 
 
+df = DataFrame( Arrow.Table("./data/processed/subtropic_sc_label_hourly_clim.arrow") )
+rename!(df, :Cloud_Optical_Thickness_mean => :cop, :Cloud_Top_Pressure_mean => :ctp, :Cloud_Fraction => :cf)
+
+
+
+
+
+# get the sub-daily transisions
+dft = @chain df begin
+    @select :date :hour :lat :lon :Label :blhh :ltsh
+    @orderby :date :hour
+    @by [:lat, :lon, :date] :class=first(:Label) :nextclass=last(:Label) :day_num=size(:Label)[1] :hour_diff=Hour.(last(:hour)-first(:hour)) :blh1=first(:blhh) :blh2=last(:blhh) :lts1=first(:ltsh) :lts2=last(:ltsh)
+    @subset :day_num.>1
+    @subset :class .== 35
+    #@rsubset :class.!=0 || :nextclass.!=0
+end
+
+
