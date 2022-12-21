348391a740bae78d5d58491d8fbd6a5b37e96687
jamesafranke <jfranke@uchicago.edu>
2022-12-21 16:49:29 -0600

    daily trans based on windspeed

--- 02_daily_plus_lagrangian.jl
+++ 02_daily_plus_lagrangian.jl
@@ -2,36 +2,95 @@ using Plots; gr(); Plots.theme(:default)
 using Arrow, DataFrames, DataFramesMeta, Dates, Statistics
 if occursin("AICCA", pwd()) == false cd("AICCA") else end
 
-
-
-year = 2003
-df = DataFrame( Arrow.Table( "./data/raw/yearly/$(year).arrow" ) )
-@select! df :Timestamp :lat :lon :Label
-df.Timestamp = DateTime.(df.Timestamp, "yyyy-mm-dd HH:MM:SS")
-@transform! df :date = Date.(:Timestamp) :hour=Hour.(:Timestamp)
-
-for col in eachcol(df) replace!( col, NaN => missing ) end
-@transform! df @byrow :Timestamp=:Timestamp[1:19]
-
-dfe = DataFrame( Arrow.Table( "./data/raw/era5/era5_$(year)_daily_blh.arrow" ) )
-dropmissing!(dfe)
-
-
-
-
-
-
-
-
-
-# get the sub-daily transisions
-dft = @chain df begin
-    @select :date :hour :lat :lon :Label :blhh :ltsh
-    @orderby :date :hour
-    @by [:lat, :lon, :date] :class=first(:Label) :nextclass=last(:Label) :day_num=size(:Label)[1] :hour_diff=Hour.(last(:hour)-first(:hour)) :blh1=first(:blhh) :blh2=last(:blhh) :lts1=first(:ltsh) :lts2=last(:ltsh)
-    @subset :day_num.>1
-    @subset :class .== 35
-    #@rsubset :class.!=0 || :nextclass.!=0
+for year in 2000:2021 
+    nothing
 end
 
+year = 2003
+### load in class data and subset to only the subtropics and load in wind speed from era5 ####
+df = DataFrame( Arrow.Table( "./data/raw/yearly/$(year).arrow" ) )
+df.Timestamp = DateTime.(df.Timestamp, "yyyy-mm-dd HH:MM:SS")
+df.Timestamp = round.(df.Timestamp, Hour(1))
+df = @chain df begin
+    @select :Timestamp :lat :lon :Label
+    dropmissing()
+    @transform :date = Date.(:Timestamp)
+    @subset :lat.<40 :lat.>-40
+end
 
+era = @chain DataFrame( Arrow.Table( "./data/raw/era5/era5_$(year)_daily_ws.arrow" ) ) begin
+    @subset :lat.<40 :lat.>-40
+    @transform :date = Date.(:time)
+    @rtransform :lon = :lon .> 180 ? :lon .- 360 : :lon
+end
+
+dates = unique(era.date)
+for date in dates
+
+
+### loop through to get all transions for a certain class ###
+dft = @subset df :Label.==35
+dft = first(dft, 10)
+out = []
+
+for row in eachrow(dft)
+    #temp_era = @subset era :date.==row.date
+    lat = row.lat
+    lon = row.lon
+    time = row.Timestamp
+
+    for i in 1:48
+        ws = @chain era begin
+            @transform :euclid = sqrt.( (:lat.-lat).^2 + (:lon.-lon).^2 )
+            @orderby :euclid
+            first(1) 
+        end
+        
+        lon = lon .+ ws.u .* 3600.0./111319.488cos.(lat)
+        lat = lat .+ ws.v .* 3600.0./111319.488
+        time = time .+ Hour(1)
+
+        temp2 = @subset df :Timestamp.==time
+        if size(temp2)[1] > 0
+            @transform! temp2 :euclid = sqrt.( (:lat.-temp.lat).^2 + (:lon.-temp.lon).^2 )
+            @subset! temp2 :euclid.<0.7
+            if size(temp2)[1] > 0
+                append!(out, temp2.Label)
+                break 
+            end
+        end
+    end
+end 
+
+
+
+
+
+
+
+
+
+
+
+for row in eachrow(dft)
+    temp_era = @subset era :date.==temp.date
+
+    for i in 1:48
+        ws = @chain temp_era begin
+            @transform :euclid = sqrt.( (:lat.-temp.lat).^2 + (:lon.-temp.lon).^2 )
+            @orderby :euclid
+            first(1) end
+        
+        @transform! temp :lat=:lat.+(ws.v.*3600.0./111319.488) :lon=:lon.+(ws.u.*3600.0./111319.488cos.(:lat)) :Timestamp=:Timestamp+Hour(1)
+
+        temp2 = @subset df :Timestamp.==temp.Timestamp
+        if size(temp2)[1] > 0
+            @transform! temp2 :euclid = sqrt.( (:lat.-temp.lat).^2 + (:lon.-temp.lon).^2 )
+            @subset! temp2 :euclid.<0.7
+            if size(temp2)[1] > 0
+                append!(out, temp2.Label)
+                break 
+            end
+        end
+    end
+end 
