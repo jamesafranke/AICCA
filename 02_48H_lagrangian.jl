74cf49b8877f8cb902ebad6020019fe03c0b7ef3
jamesafranke <jfranke@uchicago.edu>
2022-12-23 09:14:11 -0600

    rename

--- 02_48H_lagrangian.jl
+++ 02_48H_lagrangian.jl
@@ -1,35 +1,35 @@
-using Plots; gr(); Plots.theme(:default)
-using Arrow, DataFrames, DataFramesMeta, Dates, Statistics, ProgressMeter
+using Arrow, DataFrames, DataFramesMeta, Dates, ProgressMeter
 if occursin("AICCA", pwd()) == false cd("AICCA") else end
 round_step(x, step) = round(x / step) * step
 
-@showprogress for year in 2000:2021 
-    ### load in class data and subset to only the subtropics and load in wind speed from era5 ####
+for year in 2000:2021 ### load in class data and wind speed from era5 and calc transitions ####
+    print("starting----------------------------", year)
+    out = DataFrame()
+
     df = @chain DataFrame( Arrow.Table( "./data/raw/yearly/$(year).arrow" ) ) begin
         @select :Timestamp :lat :lon :Label
         @subset :lat.<40 :lat.>-40
         dropmissing()
-        @transfrom :Timestamp=DateTime.(:Timestamp, "yyyy-mm-dd HH:MM:SS") 
+        @transform :Timestamp=DateTime.(:Timestamp, "yyyy-mm-dd HH:MM:SS") 
         @transform :time_0=:Timestamp
         @transform :Timestamp=round.(:Timestamp, Hour(1))
         @transform :date = Date.(:Timestamp) end
 
     era = @chain DataFrame( Arrow.Table( "./data/raw/era5/era5_$(year)_daily_ws.arrow" ) ) begin
         @subset :lat.<40 :lat.>-40
-        @transform :date = Date.(:time)
         @rtransform :lon = :lon .> 180 ? :lon .- 360 : :lon 
+        @transform :date = Date.(:time)
         @select :date :lat :lon :u :v
         rename( :lat=>:latr, :lon=>:lonr) end
 
-    out = DataFrame()
-    for date in unique(era.date)
+    @showprogress for date in unique(era.date)
         erat = @subset era :date.<=date.+Day(2) :date.>=date
         dft = @subset df :date.==date
         future = @chain df begin
         @subset :date.<=date.+Day(2) :date.>=date
         @transform :latr = round.(Int, :lat) :lonr = round.(Int, :lon)
         @select :Timestamp :lat :lon :latr :lonr :Label
-        rename( :Label=>:next_label, :lat=>:final_lat, :lon=>:final_lon) end
+        rename( :Label=>:next_label, :lat=>:latf, :lon=>:lonf) end
 
         for i in 1:48
             dft = @chain dft begin
@@ -37,7 +37,7 @@ round_step(x, step) = round(x / step) * step
             @rtransform :latr = :latr .== -0.0 ? 0.0 : :latr :lonr = :lonr .== -0.0 ? 0.0 : :lonr
             innerjoin(erat, on = [:date, :latr, :lonr])
             @transform :lon=:lon.+:u.*3600.0./111319.488cos.(:lat) :lat=:lat.+:v.*3600.0./111319.488 :Timestamp=:Timestamp.+Hour(1)
-            @select :Timestamp :lat :lon :Label :time_0
+            @select :time_0 :Timestamp :lat :lon :Label
             @transform :latr=round.(Int, :lat) :lonr=round.(Int,:lon) :date=Date.(:Timestamp) end
 
             temp = innerjoin(dft, future, on =[:Timestamp, :latr, :lonr] )
@@ -47,5 +47,5 @@ round_step(x, step) = round(x / step) * step
             end
         end
     end
-    Arrow.write("./data/processed/transitions/$(year)_transitions_40N40S.arrow", out)
+    Arrow.write("./data/processed/transitions/$(year)_transitions_40NS.arrow", out)
 end
